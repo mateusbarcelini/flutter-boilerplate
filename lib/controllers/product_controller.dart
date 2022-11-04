8fe7fe10d238c148fd125d4cf7a69749f89066f8
Mateus Barcelini <barcelini.mateus@gmail.com>
2022-11-04 16:36:43 -0300

    Import problems

--- lib/controllers/product_controller.dart
+++ lib/controllers/product_controller.dart
@@ -1,9 +1,8 @@
 import 'package:get/state_manager.dart';
-import 'package:portugal/models/product_model.dart';
-import 'package:portugal/repositories/ProductRepository.dart';
+import 'package:flutter_boilerplate/models/product_model.dart';
+import 'package:flutter_boilerplate/repositories/ProductRepository.dart';
 
-class ProductController extends GetxController{
-
+class ProductController extends GetxController {
   RxList<Product> products = <Product>[].obs;
   RxBool load = false.obs;
 
--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
-import 'package:portugal/controllers/product_controller.dart';
-import 'package:portugal/utils/create_%20material_color.dart';
-import 'package:portugal/views/home_page.dart';
+import 'package:flutter_boilerplate/controllers/product_controller.dart';
+import 'package:flutter_boilerplate/utils/create_%20material_color.dart';
+import 'package:flutter_boilerplate/views/home_page.dart';
 
 import 'models/constans_colors.dart';
 
 void main() {
-
-  Get.lazyPut(()=>ProductController());
+  Get.lazyPut(() => ProductController());
 
   runApp(const MyApp());
 }
@@ -24,20 +23,18 @@ class MyApp extends StatelessWidget {
     return GetMaterialApp(
       title: 'Shopping',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
           primarySwatch: CreateMaterialColor().createMaterialColor(Color(ConstantsColors.int_primary)),
-          brightness: Brightness.light
-      ),
+          brightness: Brightness.light),
       home: HomePage(),
     );
   }
 }
-
